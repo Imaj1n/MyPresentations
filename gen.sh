2254663c2dbb94c365cbaeee923b083d93a4601d
Alief Hisyam <aliefhisyam1@gmail.com>
2026-07-04 21:30:34 +0700

    hedeh kok bisa

---
+++ gen.sh
@@ -0,0 +1,18 @@
+for f in \
+01-cover \
+02-outline \
+03-Qubit \
+04-Tranformasi_Uniter \
+05-Matriks_Densitas_NQubit \
+06-Keterbelitan-separab \
+07-Entropi \
+08-metodologi \
+09-parameterisasi-su \
+10-transformasi-su3-su4 \
+11-hasil-klasifikasi \
+12-interpretasi-su4 \
+13-kesimpulan \
+14-terima-kasih
+do
+    touch "sections/${f}.qmd"
+done
